c5cc561064d0c715dfcd07653f9811bc2e171b72
Lyam0udi <lyamoudi.ilias77@gmail.com>
2023-09-17 19:35:34 +0100

    LastUpdate

--- src/toDoListJavaClasses/Main.java
+++ src/toDoListJavaClasses/Main.java
@@ -1,7 +1,9 @@
 package toDoListJavaClasses;
 
 public class Main {
+	
     public static void main(String args[]) {
+    	
         // Create a new instance of the AppFrame class
         // This will start the to-do list application
         new AppFrame();
